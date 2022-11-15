92115deb7c08b109508dacf6e0052ed8f5055957
tead1234 <tead1234@gmail.com>
2022-11-16 08:35:23 +0900

    what is the problem??

--- server.js
+++ server.js
@@ -3,23 +3,15 @@ const app = express();
 const MongoClient = require('mongodb').MongoClient;
 // parser
 app.use(express.urlencoded({extends: true}))
+app.set('view engine', 'ejs')
 var db;
 MongoClient.connect(
     'mongodb+srv://tead1234:4NnnFq4ERhd09WO8@clusterforlearn.cjxn4ww.mongodb.net/dbfortest?retryWrites=true&w=majority',
     { useUnifiedTopology: true },
     (error, client)=>{
-       if(error) console.log(error)
-    db = client.db('dbfortest');
+    if(error) console.log(error)
     
-    db.collection('post').insertOne(
-        {
-            이름: 'john',
-            _id : 'jone'
-        },
-        (error, res)=>{
-            console.log(error);
-        }
-    )
+    db = client.db('dbfortest');
 
     app.listen(8080, function() {
         console.log('listening on 8080')
@@ -28,8 +20,30 @@ MongoClient.connect(
 app.get('/write', (request, res)=>{
     res.sendFile(__dirname+'/html/write.html')
 })
-//
+app.get('/list', (req,res)=>{
+    db.collection('post').find().toArray((error, res)=>{
+        // console.log(res);
+    })
+    // ejs파일로 res를 보내기 위해 작명이 필요
+    res.render('list.ejs', {posts : res},(err, res)=>{
+        console.log(err);
+    })
+
+    // res.sendFile(__dirname+'/html/list.html');
+});
 app.post('/add', (req,res)=>{
-    res.send('전송성공')
-    console.log(req.body.title)
-})
+    // res.send('전송성공')
+    // console.log(req.body.title)
+    db.collection('post').insertOne(
+        {
+            이름: req.body.title,
+            날짜: req.body.date
+        },
+        (error, res)=>{
+            console.log(error);
+        }
+
+    )
+});
+
+//
